9e14f6e0f71db9846d169ddf102a9548527ba6ef
orbitalsm3ga <66611983+orbitalsm3ga@users.noreply.github.com>
2022-09-23 12:38:10 -0700

    Object detection test

--- Turtlebot/turtlebotLinearObjDet.py
+++ Turtlebot/turtlebotLinearObjDet.py
@@ -11,11 +11,13 @@ import rospy
 from geometry_msgs.msg import Twist, Point
 from sensor_msgs.msg import LaserScan 
 from nav_msgs.msg import Odometry
+from zed_interfaces.msg import Object
+
 rospy.init_node('square')
 var = Twist()
 odom = Odometry()
 goal = Point()
-
+object = Object()
 
 
 def stop():
@@ -23,10 +25,10 @@ def stop():
     var.angular.z = 0.0
     pub.publish(var)
 
-def turn_left():
-    var.linear.x = .2
-    var.angular.z = 0.3
-    pub.publish(var)
+#def turn_left():
+#    var.linear.x = .2
+#    var.angular.z = 0.3
+#    pub.publish(var)
 
 def forward_one_meter():
     one_meter = 1
@@ -45,29 +47,33 @@ def forward_one_meter():
     pub.publish(var)
     #   i == False
 
-def turn_90():
-    degree = 1.5
-    var.angular.z = .22
-    i = 1
-    time1 = rospy.Time.now().to_sec()
-    #while i == True:
-
-    start_dist = 0
-    while start_dist < degree:
-        pub.publish(var)
-        time2 = rospy.Time.now().to_sec()
-        start_dist = .2 * (time2 - time1)
-    #i == False
-    var.angular.z = 0
-    pub.publish(var)
+#def turn_90():
+#    degree = 1.5
+#    var.angular.z = .22
+#    i = 1
+#    time1 = rospy.Time.now().to_sec()
+#    #while i == True:
+#
+#    start_dist = 0
+#    while start_dist < degree:
+#        pub.publish(var)
+#        time2 = rospy.Time.now().to_sec()
+#        start_dist = .2 * (time2 - time1)
+#    #i == False
+#    var.angular.z = 0
+#    pub.publish(var)
 
 
 def callback(msg):
     while not rospy.is_shutdown():
         for i in range(4):
             forward_one_meter()
-            turn_90()
-        rospy.is_shutdown()
+
+            if (object.position[0] >= 1):
+                print("Object detected! Stopping.")
+                stop()
+
+
 
 
 
@@ -77,5 +83,6 @@ def callback(msg):
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 sub  = rospy.Subscriber('/odom', Odometry, callback)
+objSub = rospy.Subscriber('/Object')
 rate = rospy.Rate(5)
 rospy.spin()
