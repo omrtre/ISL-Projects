d68967b136e75d706e0686bc1b15f9bd618011e7
orbitalsm3ga <66611983+orbitalsm3ga@users.noreply.github.com>
2022-09-26 11:30:10 -0700

    Node for listening.

--- Turtlebot/turtlebotLinearObjDet.py
+++ Turtlebot/turtlebotLinearObjDet.py
@@ -8,44 +8,55 @@ from turtle import distance, forward, position, speed
 
 from numpy import empty
 import rospy
-from geometry_msgs.msg import Twist, Point
-from sensor_msgs.msg import LaserScan 
-from nav_msgs.msg import Odometry
+#from geometry_msgs.msg import Twist, Point
+#from nav_msgs.msg import Odometry
 from zed_interfaces.msg import Object
 
-rospy.init_node('square')
-var = Twist()
-odom = Odometry()
-goal = Point()
-object = Object()
+postObjx = zed_interfaces.msg.Object().position[0]
 
+def callback(msg):
+    while not rospy.is_shutdown():
+        objXThreshold = 2
+        # If objects position (X) is greater than this, then sotp moving.
+        if (postObjx >= objXThreshold):
+            print("Object detected, position close by.")
 
-def stop():
-    var.linear.x = 0.0
-    var.angular.z = 0.0
-    pub.publish(var)
+def listenerObjX():
+
+    rospy.init_node('listenerObjx', anonymous = True)
+    rospy.Subscriber('/zed2i/zed_node/obj_det/objects', Object, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listenerObjX()
+
+#def stop():
+#    var.linear.x = 0.0
+#    var.angular.z = 0.0
+#    pub.publish(var)
 
 #def turn_left():
 #    var.linear.x = .2
 #    var.angular.z = 0.3
 #    pub.publish(var)
 
-def forward_one_meter():
-    one_meter = 1
-    var.linear.x = .2
-    i = 1
-    time1 = rospy.Time.now().to_sec()
-    #while i == True:
+#def forward_one_meter():
+#    one_meter = 1
+#    var.linear.x = .2
+#    i = 1
+#    time1 = rospy.Time.now().to_sec()
+#    #while i == True:
+#
+#    start_dist = 0
+#    while start_dist < one_meter:
+#        pub.publish(var)
+#        time2 = rospy.Time.now().to_sec()
+#        start_dist = .2 * (time2 - time1)
 
-    start_dist = 0
-    while start_dist < one_meter:
-        pub.publish(var)
-        time2 = rospy.Time.now().to_sec()
-        start_dist = .2 * (time2 - time1)
-
-    var.linear.x = 0
-    pub.publish(var)
-    #   i == False
+#    var.linear.x = 0
+#    pub.publish(var)
+#    #   i == False
 
 #def turn_90():
 #    degree = 1.5
@@ -63,27 +74,7 @@ def forward_one_meter():
 #    var.angular.z = 0
 #    pub.publish(var)
 
-
-def callback(msg):
-    while not rospy.is_shutdown():
-        for i in range(4):
-            forward_one_meter()
-            
-            # If objects position (X) is greater than this, then sotp moving.
-            if (object.position[0] >= 1):
-                print("Object detected! Stopping.")
-                stop()
-
-
-
-
-
-#    rospy.timer(rospy.Duration(4), straight())
-#    rospy.timer(rospy.Duration(4), turn_left())
-
-
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-sub  = rospy.Subscriber('/odom', Odometry, callback)
-objSub = rospy.Subscriber('/Object')
-rate = rospy.Rate(5)
-rospy.spin()
+#pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+#sub  = rospy.Subscriber('/odom', Odometry, callback)
+#rate = rospy.Rate(5)
+#rospy.spin()
